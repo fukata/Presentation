49e08bedb37812721b713dd4b933251692e2383d
Tatsuya Fukata <tatsuya.fukata@gmail.com>
2011-10-19 00:31:55 +0900

    implementation page move

---
+++ theme/default/main.js
@@ -0,0 +1,36 @@
+$(function(){
+	var page = parseInt(location.hash.replace('#',''), 10) || 0;
+	page = move(page);
+
+	$(window).keyup(function(ev){
+		var code = ev.keyCode;
+		switch (code) {
+			case 38: // up
+			case 75: // k
+				page = prev(page);
+				break;
+			case 40: // down 
+			case 74: // j
+				page = next(page);
+				break;
+		}
+	});
+
+	function prev(p) {
+		if (p > 0) p = move(p - 1);
+		return p;
+	}
+
+	function next(p) {
+		if (p + 1 < $('div.contents').size()) p = move(p + 1);
+		return p;
+	}
+
+	function move(p) {
+		if (isNaN(p) || p < 0 || $('div.contents').size() <= p) p = 0;
+		$('div.contents').hide();
+		$('#content-'+p).show();
+		location.hash = p;
+		return p;
+	}
+});
