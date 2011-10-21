326982d04b69c157473d9679ef51b308afad2c5a
Tatsuya Fukata <tatsuya.fukata@gmail.com>
2011-10-22 02:05:50 +0900

    add js

---
+++ js/presentation.js
@@ -0,0 +1,11 @@
+(function(window) {
+function Presentaiton() {
+}
+
+Presentaiton.prototype.move(p) {}
+Presentaiton.prototype.next(p) {}
+Presentaiton.prototype.prev(p) {}
+Presentaiton.prototype.totalPage() {}
+Presentaiton.prototype.hasPage(p) {}
+
+})(window);
